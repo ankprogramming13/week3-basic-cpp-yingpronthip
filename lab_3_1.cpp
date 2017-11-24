9aac4a9ffa19aa0532bfb395916fee77327884c6
Unknown <pronthipza@gmail.com>
2017-11-24 13:46:18 +0700

    ข้อ 1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,6 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+  cout << "Miss."<< endl<<  "Pronthip"endl << "Winyan"endl <<"Ying"endl <<"Femal"endl <<"03/12/41" endl<<"readbook" endl<<"alone"endl
   return 0;
 }
